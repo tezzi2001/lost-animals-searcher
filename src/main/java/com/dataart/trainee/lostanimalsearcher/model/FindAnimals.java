d0518bc5832734813d7fef7c423a54ac1b1aa710
IrynaDI <dmf5107@gmail.com>
2020-08-09 22:32:53 +0300

    add Animals,FindAnimals,LostAnimals,SimpleUse,UserRole

--- src/main/java/com/dataart/trainee/lostanimalsearcher/model/FindAnimals.java
+++ src/main/java/com/dataart/trainee/lostanimalsearcher/model/FindAnimals.java
@@ -3,16 +3,14 @@ package com.dataart.trainee.lostanimalsearcher.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity(name = "ads_found")
 @Data
 @NoArgsConstructor
 public class FindAnimals {
     @Id
+    @GeneratedValue
     Long id;
 
     @ManyToOne
--- src/main/java/com/dataart/trainee/lostanimalsearcher/model/LostAnimals.java
+++ src/main/java/com/dataart/trainee/lostanimalsearcher/model/LostAnimals.java
@@ -4,16 +4,14 @@ package com.dataart.trainee.lostanimalsearcher.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity(name = "ads_lost")
 @Data
 @NoArgsConstructor
 public class LostAnimals {
     @Id
+    @GeneratedValue
     Long id;
 
 
