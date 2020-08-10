d75fa401e2d3e2e734d0735df7e9ef73a938397f
Kateryna Nemchenko <nemochka99@gmail.com>
2020-08-10 13:33:39 +0300

    Basic User DTO

---
+++ src/main/java/com/dataart/trainee/lostanimalsearcher/dto/UserDTO.java
@@ -0,0 +1,10 @@
+package com.dataart.trainee.lostanimalsearcher.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private Long id;
+    private String login;
+    private String name;
+}
