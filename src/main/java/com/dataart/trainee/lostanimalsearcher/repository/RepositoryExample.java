f9575e94172598848b124d094a93a0dd827ec89d
Kateryna Nemchenko <nemochka99@gmail.com>
2020-08-10 13:54:02 +0300

    Example Files Removed

--- src/main/java/com/dataart/trainee/lostanimalsearcher/repository/RepositoryExample.java
+++
@@ -1,9 +0,0 @@
-package com.dataart.trainee.lostanimalsearcher.repository;
-
-import com.dataart.trainee.lostanimalsearcher.entity.EntityExample;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositoryExample extends JpaRepository<EntityExample, Long> {
-}
