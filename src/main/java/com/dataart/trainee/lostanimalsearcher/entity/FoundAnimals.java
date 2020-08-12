b192d99e8f3459af5acf9560abc55018b301bcf0
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2020-08-12 15:34:54 +0300

    add table to persist found animals

---
+++ src/main/java/com/dataart/trainee/lostanimalsearcher/entity/FoundAnimals.java
@@ -0,0 +1,31 @@
+package com.dataart.trainee.lostanimalsearcher.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+public class FoundAnimals {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @Lob
+    private byte[] photo;
+
+    @OneToOne
+    private User user;
+}
