c6685445bfdcab02d523ad7db9b9050fb546eb50
Kateryna Nemchenko <nemochka99@gmail.com>
2020-09-06 00:33:55 +0300

    Replaced lost animals and found animals announcement repositories with single repository for announcements entity

--- src/main/java/com/dataart/trainee/lostanimalsearcher/dao/AnnouncementRepository.java
+++ src/main/java/com/dataart/trainee/lostanimalsearcher/dao/AnnouncementRepository.java
@@ -1,9 +1,9 @@
 package com.dataart.trainee.lostanimalsearcher.dao;
 
-import com.dataart.trainee.lostanimalsearcher.entity.FoundAnimals;
+import com.dataart.trainee.lostanimalsearcher.entity.Announcement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FoundAnimalsRepository extends JpaRepository<FoundAnimals, Long> {
+public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
 }
--- src/main/java/com/dataart/trainee/lostanimalsearcher/dao/LostAnimalsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dataart.trainee.lostanimalsearcher.dao;
-
-import com.dataart.trainee.lostanimalsearcher.entity.LostAnimals;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LostAnimalsRepository extends JpaRepository<LostAnimals, Long> {
-}
