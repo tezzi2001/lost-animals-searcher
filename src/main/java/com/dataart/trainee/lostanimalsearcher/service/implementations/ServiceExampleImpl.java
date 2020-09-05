a6f6a595ab5bda40578903b9541e2b1ec50d9656
Kateryna Nemchenko <nemochka99@gmail.com>
2020-09-06 00:24:27 +0300

    Removed Example Classes

--- src/main/java/com/dataart/trainee/lostanimalsearcher/service/implementations/ServiceExampleImpl.java
+++
@@ -1,8 +0,0 @@
-package com.dataart.trainee.lostanimalsearcher.service.implementations;
-
-import com.dataart.trainee.lostanimalsearcher.service.ServiceExample;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ServiceExampleImpl implements ServiceExample {
-}
