edc5ff5d3d78c0a5990070b70a81c9af635d96d0
Kateryna Nemchenko <nemochka99@gmail.com>
2020-09-12 12:13:29 +0300

    AnnouncementServiceImpl class implemented

--- src/main/java/com/dataart/trainee/lostanimalsearcher/service/implementations/AnnouncementServiceImpl.java
+++ src/main/java/com/dataart/trainee/lostanimalsearcher/service/implementations/AnnouncementServiceImpl.java
@@ -1,55 +1,97 @@
 package com.dataart.trainee.lostanimalsearcher.service.implementations;
 
 import com.dataart.trainee.lostanimalsearcher.dao.AnnouncementRepository;
+import com.dataart.trainee.lostanimalsearcher.dao.UserRepository;
 import com.dataart.trainee.lostanimalsearcher.entity.Announcement;
+import com.dataart.trainee.lostanimalsearcher.entity.AnnouncementType;
+import com.dataart.trainee.lostanimalsearcher.entity.User;
 import com.dataart.trainee.lostanimalsearcher.service.AnnouncementService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class AnnouncementServiceImpl implements AnnouncementService {
 
     @Autowired
     AnnouncementRepository announcementRepository;
+    @Autowired
+    UserRepository userRepository;
 
 
     @Override
     public Announcement getAnnouncementById(Long id) {
-        return null;
+        return announcementRepository.findById(id).orElse(null);
     }
 
     @Override
     public List<Announcement> getAll() {
-        return null;
+        List<Announcement> announcements = announcementRepository.findAll();
+        return announcements.isEmpty() ? new ArrayList<>() : announcements;
     }
 
     @Override
-    public List<Announcement> getAllByType(String type) {
-        return null;
+    public List<Announcement> getAllByType(AnnouncementType type) {
+        List<Announcement> announcements = announcementRepository.findAllByType(type);
+        return announcements.isEmpty() ? new ArrayList<>() : announcements;
     }
 
     @Override
-    public List<Announcement> getAllByUserId(Long user) {
-        return null;
+    public List<Announcement> getAllByUserId(Long userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            List<Announcement> announcements = announcementRepository.findAllByUser(userOptional.get());
+            return announcements.isEmpty() ? new ArrayList<>() : announcements;
+        }
+        return new ArrayList<>();
     }
 
     @Override
-    public List<Announcement> getAllByUserIdAndType(Long user, String type) {
-        return null;
+    public List<Announcement> getAllByUserIdAndType(Long userId, AnnouncementType type) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            List<Announcement> announcements = announcementRepository
+                    .findAllByUserAndType(userOptional.get(), type);
+            return announcements.isEmpty() ? new ArrayList<>() : announcements;
+        }
+        return new ArrayList<>();
     }
 
     @Override
     public boolean addAnnouncement(Announcement announcement) {
+        if (announcement != null) {
+            announcementRepository.save(announcement);
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean closeAnnouncement(Announcement announcement) {
+        if (announcement != null && announcement.getId() != null) {
+            Optional<Announcement> announcementOptional = announcementRepository
+                    .findById(announcement.getId());
+            if (announcementOptional.isPresent()) {
+                Announcement closedAnnouncement = announcementOptional.get();
+                closedAnnouncement.setActive(false);
+                announcementRepository.save(closedAnnouncement);
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean deleteAnnouncement(Announcement announcement) {
+        if (announcement != null && announcement.getId() != null) {
+            Optional<Announcement> announcementOptional = announcementRepository
+                    .findById(announcement.getId());
+            if (announcementOptional.isPresent()) {
+                announcementRepository.delete(announcementOptional.get());
+                return true;
+            }
+        }
         return false;
     }
 }
